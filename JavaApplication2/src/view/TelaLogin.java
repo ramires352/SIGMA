0f8b15fcb5a5bb113052c02be32a8a6aae002481
Raul Ramires <ramires1432@outlook.com>
2018-09-03 14:20:10 -0300

    Documento de Requisitos, Product Backlog e DER

--- JavaApplication2/src/view/TelaLogin.java
+++ JavaApplication2/src/view/TelaLogin.java
@@ -150,7 +150,7 @@ public class TelaLogin extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
         );
 
         pack();
