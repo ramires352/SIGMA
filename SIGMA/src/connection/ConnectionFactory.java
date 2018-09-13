9fe395046c5824ccee5315ec09ba6e3719e66838
Raul Ramires <ramires1432@outlook.com>
2018-09-13 17:06:11 -0300

    Update ConnectionFactory

--- SIGMA/src/connection/ConnectionFactory.java
+++ SIGMA/src/connection/ConnectionFactory.java
@@ -25,8 +25,8 @@ public class ConnectionFactory {
     private static final String PASS = "RdH13MiLTE";*/
     
     //BD Local
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/sigmaBD";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/sigmaBD?useSSL=false";
     private static final String USER = "root";
     private static final String PASS = "ramires2313";
     
