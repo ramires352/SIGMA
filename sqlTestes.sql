9ce170a799c942d34658a26051a834f475691064
Raul Ramires <ramires1432@outlook.com>
2018-09-27 15:20:50 -0300

    SQL testes

---
+++ sqlTestes.sql
@@ -0,0 +1,18 @@
+select * from terreno;
+
+select * from produto;
+
+select if((select cultura from terreno where idTerreno = 1) in (select tipo from produto where login = 'teste'),'TRUE','FALSE');
+
+select qtde from produto where tipo = 'Soja';
+update produto set qtde = qtde + 10 where tipo = 'Soja' and login = 'teste';
+
+select * from movimento;
+
+select * from colheita;
+
+select * from colheita where data > '2018-09-13';
+
+select * from colheita where idTerreno in (select idTerreno from terreno where login = 'teste') and data >= '2018-09-13' and data <= '2018-09-14';
+
+insert into movimento (nome, tipo, qtde, preco_un, login, data) values ("testando","Semente",10,45.67,"teste",'2018-09-19');
