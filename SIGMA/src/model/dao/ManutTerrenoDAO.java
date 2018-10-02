f4b7a753910700ea4cac6f3069e77eb8fe619103
Raul Ramires <ramires1432@outlook.com>
2018-10-02 20:28:56 -0300

    ManutTerrenoDAO.read()

--- SIGMA/src/model/dao/ManutTerrenoDAO.java
+++ SIGMA/src/model/dao/ManutTerrenoDAO.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
+import model.bean.Cliente;
 import model.bean.ManutTerreno;
 
 /**
@@ -30,6 +31,76 @@ public class ManutTerrenoDAO {
         try{
             stnt = con.prepareStatement("SELECT * FROM manut_terreno WHERE idTerreno = ?");
             stnt.setInt(1, id);
+            
+            rs = stnt.executeQuery();
+            
+            while(rs.next()){
+                ManutTerreno mt = new ManutTerreno();
+                
+                mt.setIdManut_terreno(rs.getInt("idManut_terreno"));
+                mt.setNome(rs.getString("nome"));
+                mt.setDefensivo(rs.getString("defensivo"));
+                mt.setData(rs.getDate("data"));
+                mt.setIdTerreno(rs.getInt("idTerreno"));
+                
+                manutTerrenos.add(mt);
+            }
+            
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro na Leitura! "+ ex);
+        }
+        finally{
+            ConnectionFactory.closeConnection(con, stnt, rs);
+        }
+        return manutTerrenos;
+            
+    }
+    
+    public List<ManutTerreno> read(){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stnt = null;
+        ResultSet rs = null;
+        List<ManutTerreno> manutTerrenos = new ArrayList<>();
+        
+        try{
+            stnt = con.prepareStatement("SELECT * FROM manut_terreno WHERE login = ?");
+            stnt.setString(1, Cliente.getNome());
+            
+            rs = stnt.executeQuery();
+            
+            while(rs.next()){
+                ManutTerreno mt = new ManutTerreno();
+                
+                mt.setIdManut_terreno(rs.getInt("idManut_terreno"));
+                mt.setNome(rs.getString("nome"));
+                mt.setDefensivo(rs.getString("defensivo"));
+                mt.setData(rs.getDate("data"));
+                mt.setIdTerreno(rs.getInt("idTerreno"));
+                
+                manutTerrenos.add(mt);
+            }
+            
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Erro na Leitura! "+ ex);
+        }
+        finally{
+            ConnectionFactory.closeConnection(con, stnt, rs);
+        }
+        return manutTerrenos;
+            
+    }
+    
+    public List<ManutTerreno> read(){
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stnt = null;
+        ResultSet rs = null;
+        List<ManutTerreno> manutTerrenos = new ArrayList<>();
+        
+        try{
+            stnt = con.prepareStatement("SELECT * FROM manut_terreno WHERE login = ?");
+            stnt.setString(1, Cliente.getNome());
             
             rs = stnt.executeQuery();
             
