f3b5a270b9c4343799be77aa4f6fcaa93e5d6101
Raul Ramires <ramires1432@outlook.com>
2018-10-18 11:05:10 -0300

    Update sqlTestes

--- sqlTestes.sql
+++ sqlTestes.sql
@@ -16,3 +16,5 @@ select * from colheita where data > '2018-09-13';
 select * from colheita where idTerreno in (select idTerreno from terreno where login = 'teste') and data >= '2018-09-13' and data <= '2018-09-14';
 
 insert into movimento (nome, tipo, qtde, preco_un, login, data) values ("testando","Semente",10,45.67,"teste",'2018-09-19');
+
+select * from produto where login = 'teste' and tipo in ('Soja','Milho','Aveia','Trigo');
